50e8e91eb702cc7a799c4268a4496ef11beb3722
Kylee Tilley <kyleetilley@gmail.com>
2021-07-05 03:04:34 -0500

    Add plant form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,76 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export async function load({ fetch }) {
+		const plants = await loadPlants(fetch);
+
+		if (plants) {
+			return {
+				props: { plants }
+			};
+		}
+	}
+</script>
+
+<script>
+	export let plants = [];
+
+	let name;
+	let schedule;
+
+	async function onSubmit(e) {
+		e.preventDefault();
+
+		await fetch('/plants', {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ name, schedule })
+		}).then((res) => res.json());
+
+		name = '';
+		schedule = '';
+
+		plants = (await loadPlants(fetch)) ?? [];
+	}
+
+	async function loadPlants(fetch) {
+		const url = `/plants`;
+		const res = await fetch(url);
+
+		if (res.ok) {
+			return await res.json();
+		}
+	}
+</script>
+
+<h1>Plants {plants.length}</h1>
+
+<form on:submit={onSubmit}>
+	<div>
+		<label for="name">Name</label>
+		<input type="text" name="name" bind:value={name} required />
+	</div>
+	<div>
+		<label for="schedule">Schedule</label>
+		<input type="text" name="schedule" bind:value={schedule} required />
+	</div>
+	<button>Add</button>
+</form>
+
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Schedule</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each plants as plant}
+			<tr>
+				<td>{plant.name}</td>
+				<td>{plant.schedule}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
---
+++ src/routes/plants.ts
@@ -0,0 +1,23 @@
+export async function get() {
+    const result = await fetch('http://localhost:3001/plant').then(res => res.json());
+
+    return {
+        body: result
+    };
+}
+
+export async function post(request) {
+    console.log(request.body);
+    const result = await fetch('http://localhost:3001/plant', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request.body)
+    }).then(res => res.json());
+
+    return {
+        body: result
+    };
+}
