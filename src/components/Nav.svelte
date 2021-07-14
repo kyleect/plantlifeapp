c763d85ae15fd31a35f4d023f4f15dced3e64d61
Kylee Tilley <kyleetilley@gmail.com>
2021-07-14 12:38:22 -0500

    Change current nav to be bold

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,7 +1,34 @@
+<script>
+	import { page } from '$app/stores';
+
+	$: console.log($page.path);
+</script>
+
 <nav class="border-b-2">
-	<ul class="w-full inline-flex justify-around text-center font-bold">
-		<li class="w-full"><a href="/" class="block p-5">Home</a></li>
-		<li class="w-full"><a href="/plants" class="block p-5">Plants</a></li>
-		<li class="w-full"><a href="/watering" class="block p-5">Watering</a></li>
+	<ul class="w-full inline-flex justify-around text-center">
+		<li class="w-full">
+			<a href="/" class={`block p-5 hover:bg-gray-200 ${$page.path === '/' ? 'active' : ''}`}
+				>Home</a
+			>
+		</li>
+		<li class="w-full">
+			<a
+				href="/plants"
+				class={`block p-5 hover:bg-gray-200 ${$page.path === '/plants' ? 'active' : ''}`}>Plants</a
+			>
+		</li>
+		<li class="w-full">
+			<a
+				href="/watering"
+				class={`block p-5 hover:bg-gray-200 ${$page.path === '/watering' ? 'active' : ''}`}
+				>Watering</a
+			>
+		</li>
 	</ul>
 </nav>
+
+<style>
+	.active {
+		font-weight: bold;
+	}
+</style>
