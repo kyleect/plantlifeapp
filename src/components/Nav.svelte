99c11db1fb8383b6674968f852b008d499b10ed0
Kylee Tilley <kyleetilley@gmail.com>
2021-07-15 19:02:10 -0500

    Add counters to nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { page } from '$app/stores';
+	import plants, { wateringPlantIds } from '../stores/plants.store';
 </script>
 
 <nav class="border-b-2">
@@ -12,14 +13,15 @@
 		<li class="w-full">
 			<a
 				href="/plants"
-				class={`block p-5 hover:bg-gray-200 ${$page.path === '/plants' ? 'active' : ''}`}>Plants</a
+				class={`block p-5 hover:bg-gray-200 ${$page.path === '/plants' ? 'active' : ''}`}
+				>Plants ({$plants.length})</a
 			>
 		</li>
 		<li class="w-full">
 			<a
 				href="/watering"
 				class={`block p-5 hover:bg-gray-200 ${$page.path === '/watering' ? 'active' : ''}`}
-				>Watering</a
+				>Watering ({$wateringPlantIds.length})</a
 			>
 		</li>
 	</ul>
